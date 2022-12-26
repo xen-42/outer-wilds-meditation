c2357b531346d37c4ea502f2d4c3eae5e58d8650
Nick <nicholasconnors@hotmail.ca>
2022-12-26 14:25:26 -0500

    Create mod

---
+++ EnableMeditation/EnableMeditation.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace EnableMeditation
+{
+	[HarmonyPatch]
+	public class EnableMeditation : ModBehaviour
+	{
+		private void Awake()
+		{
+			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(PauseMenuManager), nameof(PauseMenuManager.Start))]
+		private static void PauseMenuManager_Start(PauseMenuManager __instance)
+		{
+			__instance._skipToNextLoopButton.SetActive(true);
+		}
+	}
+}
